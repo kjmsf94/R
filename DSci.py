a2d68490a8496d49b6436cd9023c19396b961048
KEVIN MATHIS <65684330+kjmsf94@users.noreply.github.com>
2021-07-10 02:59:51 -0700

    Add files via upload

---
+++ DSci.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+#!pip install beautifulsoup4
+#import beautifulsoup4 as soup
+
+import requests # library to handle requests
+import pandas as pd # library for data analsysis
+import numpy as np # library to handle data in a vectorized manner
+import random # library for random number generation
+
+
+get_ipython().system('pip install geopy')
+from geopy.geocoders import Nominatim # module to convert an address into latitude and longitude values
+
+# libraries for displaying images
+from IPython.display import Image 
+from IPython.core.display import HTML 
+    
+# tranforming json file into a pandas dataframe library
+from pandas.io.json import json_normalize
+
+
+get_ipython().system(' pip install folium==0.5.0')
+import folium # plotting library
+
+print('Folium installed')
+print('Libraries imported.')
+
+
+
+
+import numpy as np # library to handle data in a vectorized manner
+
+import pandas as pd # library for data analsysis
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+import json # library to handle JSON files
+
+get_ipython().system("conda install -c conda-forge geopy --yes # uncomment this line if you haven't completed the Foursquare API lab")
+from geopy.geocoders import Nominatim # convert an address into latitude and longitude values
+
+import requests # library to handle requests
+from pandas.io.json import json_normalize # tranform JSON file into a pandas dataframe
+
+# Matplotlib and associated plotting modules
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+
+# import k-means from clustering stage
+from sklearn.cluster import KMeans
+
+#!conda install -c conda-forge folium=0.5.0 --yes # uncomment this line if you haven't completed the Foursquare API lab
+import folium # map rendering library
+
+print('Libraries imported.')
+
+
+# In[17]:
+
+
+
+table_contents=[]
+table=soup.find('table')
+for row in table.findAll('td'):
+    cell = {}
+    if row.span.text=='Not assigned':
+        pass
+    else:
+        cell['PostalCode'] = row.p.text[:3]
+        cell['Borough'] = (row.span.text).split('(')[0]
+        cell['Neighborhood'] = (((((row.span.text).split('(')[1]).strip(')')).replace(' /',',')).replace(')',' ')).strip(' ')
+        table_contents.append(cell)
+
+# print(table_contents)
+df=pd.DataFrame(table_contents)
+df['Borough']=df['Borough'].replace({'Downtown TorontoStn A PO Boxes25 The Esplanade':'Downtown Toronto Stn A',
+                                             'East TorontoBusiness reply mail Processing Centre969 Eastern':'East Toronto Business',
+                                             'EtobicokeNorthwest':'Etobicoke Northwest','East YorkEast Toronto':'East York/East Toronto',
+                                             'MississaugaCanada Post Gateway Processing Centre':'Mississauga'})
+
+
+# In[ ]:
+
+
+
+
